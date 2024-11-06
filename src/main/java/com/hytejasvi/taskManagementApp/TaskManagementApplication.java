fd4d25e4b4970ff8d61521712dafbe974a1ad8ac
hytejasvi <hytejasvi@gmail.com>
2024-11-06 10:54:59 +0100

    initial commit

---
+++ src/main/java/com/hytejasvi/taskManagementApp/TaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hytejasvi.taskManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+
+}
