9d8532b3bbb93d82d5484143a5b3744b7d767911
hytejasvi <hytejasvi@gmail.com>
2024-11-12 22:14:22 +0100

    Functionality added to get all tasks of a user and update a particular task

--- src/main/java/com/hytejasvi/taskManagementApp/controller/TaskController.java
+++ src/main/java/com/hytejasvi/taskManagementApp/controller/TaskController.java
@@ -2,16 +2,17 @@ package com.hytejasvi.taskManagementApp.controller;
 
 import com.hytejasvi.taskManagementApp.entity.Task;
 import com.hytejasvi.taskManagementApp.service.TaskServices;
+import jakarta.websocket.server.PathParam;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -27,5 +28,21 @@ public class TaskController {
 
         taskServices.createTask(authentication.getName(), task);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Task>> getAllTasks() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        List<Task> tasks = taskServices.fetchAllTasksForUser(userName);
+        return new ResponseEntity<>(tasks, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/updateTask/{taskId}")
+    public ResponseEntity<Task> updateTask(@RequestBody Task updatedTask, @PathVariable ObjectId taskId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        Task res = taskServices.updateTaskForUser(userName, taskId, updatedTask);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 }
