cc864bad435ccce35cb6c77b9fa546e87b5e5f9f
hytejasvi <hytejasvi@gmail.com>
2024-11-20 20:50:24 +0100

    changes to store the deadline in UTC format and enable scheduling in our application

--- src/main/java/com/hytejasvi/taskManagementApp/TaskManagementApplication.java
+++ src/main/java/com/hytejasvi/taskManagementApp/TaskManagementApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class TaskManagementApplication {
 
 	public static void main(String[] args) {
